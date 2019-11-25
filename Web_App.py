6a94277d61a741bdbce0789e38004316e5aaef4d
Manu Hegde <manu.hegde02@gmail.com>
2019-11-25 08:19:46 -0500

    test

--- Web_App.py
+++ Web_App.py
@@ -7,9 +7,9 @@ numberofClicks = 0
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/api/weather')
+def weatherInfo():
+    return 'weather'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
